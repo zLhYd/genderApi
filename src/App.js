8c9b4d45795f139e8c01e3319a91e59a340ef87e
zLhYd <adigans221@gmail.com>
2022-10-26 22:02:32 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [player, getPlayer] = useState([{}]);
+  const [search, setSearch] = useState("adi");
+
+  const changeValue = (data) => {
+    setSearch(data.target.value);
+  };
+
+  useEffect(() => {
+    const Anjay = async () => {
+      const Api = await fetch(`https://api.genderize.io/?name=${search}`);
+      const getApi = await Api.json();
+      const set = new Set([getApi]);
+      const result = Array.from(set).map((data) => {
+        return {
+          hitung: data.count,
+          jenis: data.gender,
+          nama: data.name,
+          kemungkinan: data.probability,
+        };
+      });
+
+      // console.log(result);
+      getPlayer(result);
+    };
+    Anjay();
+  }, [search]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        <span class="input">
+          <input onChange={changeValue} type="text" placeholder="NAME" />
+          <span></span>
+        </span>
+        <p key={player.hitung}>count: {player[0].hitung}</p>
+        <p key={player.nama}>name: {player[0].nama}</p>
+        <p key={player.jenis}>gender: {player[0].jenis}</p>
+        <p key={player.kemungkinan}>probability: {player[0].kemungkinan}</p>
+      </p>
     </div>
   );
-}
+};
 
 export default App;
